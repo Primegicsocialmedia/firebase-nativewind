cd26dc60fe88cdbe74e8a8d31f6ba193f993608e
Primegicsocialmedia <valentio.samuels777@gmail.com>
2024-11-05 16:54:28 +0200

    second commit

--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,59 @@
-import { Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { ScrollView, StyleSheet, Text, View, Image } from "react-native";
+import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../constants";
+import CustomButton from "../components/CustomButton";
+import { router } from "expo-router";
 
-export default function Index() {
+export default function App() {
   return (
-    <View className="flex-1 items-center bg-white">
-      <Text>Hello world!</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full flex justify-center items-center min-h-[85vh] px-4">
+          <Image
+            source={images.logo}
+            className="w-[180px] h-[120px]"
+            resizeMode="contain"
+          />
+
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode="contain"
+          />
+
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover Endless{"\n"} Possibilities with{" "}
+              <Text className="text-secondary-200">Prime</Text>
+            </Text>
+
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+              resizeMode="contain"
+            />
+          </View>
+          <Text className="text-base font-pregular text-gray-100 mt-7 text-center">
+            Where Creativity Meets Innovation: Embark on a Journey of Limitless
+            Exploration with Prime
+          </Text>
+
+          <CustomButton
+            title="Continue with Email"
+            handlePress={() => router.push("/sign-in")}
+            containerStyles="w-full mt-7"
+          />
+
+          <CustomButton
+            title="Home"
+            handlePress={() => router.push("/home")}
+            containerStyles="w-[30%]   mt-20 bg-white"
+          />
+        </View>
+      </ScrollView>
+      <StatusBar backgroundColor="#161622" style="light" />
+    </SafeAreaView>
   );
 }
